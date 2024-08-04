890f28c37ac6dcf1c835c4524429694b821fd8c7
MioChan81 <570543641@qq.com>
2024-08-04 20:31:45 +0800

    commit

--- src/CuniswapV2Pair.sol
+++ src/CuniswapV2Pair.sol
@@ -90,22 +90,59 @@ contract ZuniswapV2Pair is ERC20, Math {
         emit Burn(msg.sender, amount0, amount1);
     }
 
-    function swap(uint256 amount0Out, uint256 amount1Out, address to) public {
-        if (amount0Out == 0 && amount1Out == 0) {
-            revert InsufficientLiquidity();
-        }
+    modifier nonReentrant() {
+        require(!isEntered);
+        isEntered = true;
+        _;
+        isEntered = false;
+    }
+
+    function swap(
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address to,
+        bytes calldata data
+    ) public nonReentrant {
+        if (amount0Out == 0 && amount1Out == 0)
+            revert InsufficientOutputAmount();
 
         (uint112 reserve0_, uint112 reserve1_, ) = getReserves();
-        uint256 balance0 = IERC20(token0).balanceOf(address(this)) - amount0Out;
-        uint256 balance1 = IERC20(token1).balanceOf(address(this)) - amount1Out;
 
-        if (balance0 * balance1 < uint256(reserve0_) * uint256(reserve1_))
-            revert InvalidK();
-
-        _update(balance0, balance1, reserve0_, reserve1_);
+        if (amount0Out > reserve0_ || amount1Out > reserve1_)
+            revert InsufficientLiquidity();
 
         if (amount0Out > 0) _safeTransfer(token0, to, amount0Out);
         if (amount1Out > 0) _safeTransfer(token1, to, amount1Out);
+        if (data.length > 0)
+            ICuniswapV2Callee(to).cuniswapV2Call(
+                msg.sender,
+                amount0Out,
+                amount1Out,
+                data
+            );
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+
+        uint256 amount0In = balance0 > reserve0 - amount0Out
+            ? balance0 - (reserve0 - amount0Out)
+            : 0;
+        uint256 amount1In = balance1 > reserve1 - amount1Out
+            ? balance1 - (reserve1 - amount1Out)
+            : 0;
+
+        if (amount0In == 0 && amount1In == 0) revert InsufficientInputAmount();
+
+        // Adjusted = balance before swap - swap fee; fee stays in the contract
+        uint256 balance0Adjusted = (balance0 * 1000) - (amount0In * 3);
+        uint256 balance1Adjusted = (balance1 * 1000) - (amount1In * 3);
+
+        if (
+            balance0Adjusted * balance1Adjusted <
+            uint256(reserve0_) * uint256(reserve1_) * (1000 ** 2)
+        ) revert InvalidK();
+
+        _update(balance0, balance1, reserve0_, reserve1_);
 
         emit Swap(msg.sender, amount0Out, amount1Out, to);
     }
